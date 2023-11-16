e65f041c8b99003057d0b3cd94de9cbe349539fb
StormJo <storm_joye@hotmail.com>
2023-11-16 12:35:11 +0100

    Add Login code

---
+++ app/src/main/java/edu/ap/playcomic/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package edu.ap.playcomic.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
